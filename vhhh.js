d858d3300a549bf20a9d22863718bed3f64598cb
TimTim <i@timtim.hk>
2020-07-07 11:23:13 +0800

    Simplify vhhh.js

--- vhhh.js
+++ vhhh.js
@@ -3,36 +3,13 @@ const axios = require("axios")
 
 const vhhh_url = "http://atis.cad.gov.hk/ATIS/ATISweb/atis.php"
 
+const concat = (document, className) =>
+    [...document.querySelectorAll(className)].map(n => n.textContent.trim()).filter(t => t).join(" ");
 
-function cleanHTML(html) {
-
-    let ret = html.replace(/\n/g, " ")
-    ret = ret.replace(/\t/g, "")
-    ret = ret.replace(/[ ]+/g, " ")
-    ret = ret.replace(" ARRIVAL ", "")
-    ret = ret.replace(" DEPARTURE ", "")
-    ret = ret.replace(/[\u00A0]/g, "")
-    ret = ret.replace(/[ ]+/g, " ")
-    ret = ret.substring(0, ret.length - 1)
-    return ret
+module.exports = async function (icao) {
+    const doc = new JSDOM((await axios.get(vhhh_url)).data).window.document;
+    return {
+        arrival: concat(doc, ".data_name_arr"),
+        departure: concat(doc, ".data_name_dep")
+    };
 }
-
-async function run(icao) {
-    const response = await axios.get(vhhh_url);
-    const dom = new JSDOM(response.data);
-    const doc = dom.window.document
-    const col1 = [...doc.querySelectorAll(".data_outer_1 .width_left")];
-    const col2 = [...doc.querySelectorAll(".data_outer_3 .width_right")];
-
-    const col1text = cleanHTML(col1[0].textContent)
-    const col2text = cleanHTML(col2[0].textContent)
-    const ret = {
-        "arrival": col1text,
-        "departure": col2text,
-        "body": ""
-    }
-
-    return ret
-}
-
-module.exports = run
