d5c6923e05d1639b049f09f3078a235188b02d86
TimTim Wong <t2@live.hk>
2020-07-08 11:22:13 +0800

    Fix sandbox

--- test.ts
+++ test.ts
@@ -6,12 +6,19 @@
  * @author Timothy Wong (@wegylexy on GitHub)
  */
 import { NodeVM } from "vm2";
+const allowedModules = ["jsdom", "axios"];
 (async () => {
     console.log(JSON.parse(await new NodeVM({
         console: "off",
         eval: false,
         require: {
-            external: ["jsdom", "anxios"]
+            context: "sandbox",
+            external: true,
+            mock: allowedModules.reduce((a, m) => {
+                a[m] = require(m);
+                return a;
+            }, {} as { [name: string]: any }),
+            root: "."
         },
         wrapper: "none"
     }).run(`"use strict";return (async () => JSON.stringify(await require("${process.argv[2]}").default("${process.argv[3]}")))();`, __filename)));
